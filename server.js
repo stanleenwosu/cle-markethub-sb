15a88929b3ec86853b284b5b4fc4f43e39a820f2
Ahmed <ahmeddapo0110@gmail.com>
2021-10-02 00:11:16 +0100

    clean all

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 console.log(`RESTARTING BY: ${new Date()}`);
 const { createServer } = require('http');
 const { parse } = require('url');
-const next = require('next');
 const path = require('path');
+const next = require('next');
 
 console.log(1);
+console.log('ENV', process.env.NODE_ENV);
 const dev = process.env.NODE_ENV !== 'production';
-const pth = path.join(__dirname, '..', 'cle.ng3/server');
+const pth = path.join(__dirname, '..', 'cle.ng4');
 console.log('path:', pth);
 const app = next({ dir: pth, dev });
+console.log(2, app);
 const handle = app.getRequestHandler();
+console.log(3);
 const port = 26956;
 app.prepare().then(() => {
+  console.log(4);
   createServer((req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
