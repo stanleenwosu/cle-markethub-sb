d935edf0eb55f65645d56ec82f5f4d17e7f86bfe
Richard Saseun <richardsaseun@gmail.com>
2022-02-19 19:46:22 +0100

    chore: addnew images for slides

--- components/partials/shop/ShopBanner.jsx
+++ components/partials/shop/ShopBanner.jsx
@@ -8,6 +8,7 @@ const ShopBanner = () => {
     dots: false,
     arrows: true,
     infinite: true,
+    autoplay: true,
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -17,14 +18,19 @@ const ShopBanner = () => {
   return (
     <div className="mb-5">
       <Slider {...carouselSetting} fade={true} className="ps-carousel">
-        <img src="/static/img/mine/slide-1.png" alt="CLE" />
+        <img
+          className="img-fluid"
+          src="/static/img/mine/slide-1.png"
+          alt="CLE"
+        />
         <img src="/static/img/mine/slide-2.png" alt="CLE" />
         <img src="/static/img/mine/slide-3.png" alt="CLE" />
-        <img src="/static/img/mine/slide-4.jpg" alt="CLE" />
-        <img src="/static/img/mine/slide-5.jpg" alt="CLE" />
-        <img src="/static/img/mine/slide-6.jpg" alt="CLE" />
-        <img src="/static/img/mine/slide-7.jpg" alt="CLE" />
-        <img src="/static/img/mine/slide-8.jpg" alt="CLE" />
+        <img src="/static/img/mine/slide-4.png" alt="CLE" />
+        <img src="/static/img/mine/slide-5.png" alt="CLE" />
+        <img src="/static/img/mine/slide-6.png" alt="CLE" />
+        <img src="/static/img/mine/slide-7.png" alt="CLE" />
+        <img src="/static/img/mine/slide-8.png" alt="CLE" />
+        <img src="/static/img/mine/slide-9.png" alt="CLE" />
       </Slider>
     </div>
   );
